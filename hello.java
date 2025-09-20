7192b5ea9f5938cc50c2e43c06b203c840c2de88
Sourav Satpathy <sourav.satpathy@outlook.com>
2025-09-20 13:03:05 +0530

    remove line no. 6 from hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,5 @@ class hello{
 		System.out.println("Hello World..!!!");
 		System.out.println("Welcome to JAVA Tutorial..!!");
 		System.out.println("Welcome to GIT 101");
-		System.out.println("This is very good..!!!!!");
 	}
 }
