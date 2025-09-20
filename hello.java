a086d8d16156c00ea46be0795b5c6368d2098d69
Sourav Satpathy <sourav.satpathy@outlook.com>
2025-09-20 10:10:56 +0530

    update line 4 and 5 on hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class hello{
 	public static void main(String[] args) {
 		System.out.println("Hello World..!!!");
-		System.out.println("Welcome to JAVA Tutorial..!!\n");
-		System.out.println("Welcome to GIT 101\n");
+		System.out.println("Welcome to JAVA Tutorial..!!");
+		System.out.println("Welcome to GIT 101");
 	}
 }
