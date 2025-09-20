8188bb51819e3db174e119c4acba155d0d015542
Sourav Satpathy <sourav.satpathy@outlook.com>
2025-09-20 10:33:39 +0530

    add new line at line no. 7 on hello.java

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ class hello{
 		System.out.println("Welcome to JAVA Tutorial..!!");
 		System.out.println("Welcome to GIT 101");
 		System.out.println("This is very good..!!!!!");
+		System.out.println("Hope you will enjoy this");
 	}
 }
